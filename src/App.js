6dc136b6d585a67eca529cbfdd5c6354e78c1917
stevencainn <stevencainn@gmail.com>
2021-07-19 13:46:10 -0400

    set up axios and requests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import React from 'react';
+import Row from './Row';
+import requests from './requests';
+
 
 function App() {
   return (
     <div className="App">
       <h1>netflix clone</h1>
+      <Row title=" NETFLIX ORGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
