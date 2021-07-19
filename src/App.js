29c42fd04032f3bddeaafd48a85333908b821863
stevencainn <stevencainn@gmail.com>
2021-07-19 15:38:08 -0400

    styled dynamic rows

--- src/App.js
+++ src/App.js
@@ -9,8 +9,15 @@ function App() {
   return (
     <div className="App">
       <h1>netflix clone</h1>
-      <Row title=" NETFLIX ORGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title=" Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios'
 import requests from './requests';
+import "./Row.css"
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row( {title, fetchUrl}) {
+function Row( {title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -23,10 +24,13 @@ function Row( {title, fetchUrl}) {
             {/* title */}
             <h2>{ title }</h2>
 
-            <div className='row__posters'>
+                <div className='row__posters'>
                 {/*poster */}
                 {movies.map(movie =>(
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                    <img 
+                    key={movie.id} 
+                    className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>
                 ))}
             </div>
 
