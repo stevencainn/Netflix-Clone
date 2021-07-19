550dd007dca9ae4f4125aab2cb7526108376b292
stevencainn <stevencainn@gmail.com>
2021-07-19 15:02:03 -0400

    got axios response

--- src/Row.js
+++ src/Row.js
@@ -1,13 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from './axios'
+import requests from './requests';
 
-function Row( {title}) {
+function Row( {title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
-    
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+        
+    }, [fetchUrl])
+    console.table(movies);
 
     return (
-        <div>
+        <div className="row">
             {/* title */}
             <h2>{ title }</h2>
+
+            <div className='row__posters'>
+                {/*poster */}
+                {movies.map(movie =>(
+                    <img src={movie.poster_path} alt={movie.name}/>
+                ))}
+            </div>
+
 
             {/* container -> posters */}
         </div>
