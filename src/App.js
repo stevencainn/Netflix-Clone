c13604eea516658025bcf9e56d203dcc71f5cf23
stevencainn <stevencainn@gmail.com>
2021-07-20 17:34:39 -0400

    got trailers pop up to work but getting an error for some movies, need to mess with id of movies

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Nav />
       <Banner />
-      <Row title=" Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
+      <Row title=" Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
